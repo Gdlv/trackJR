953d48a1a23f97c51d3c1093d69c8730d3d43f78
GERARDO DE LA VEGA <delavega.gerardo@gmail.com>
2020-10-12 16:24:00 -0300

    change de encoding

--- R/juliasTrackingFunction.R
+++ R/juliasTrackingFunction.R
@@ -1,5 +1,5 @@
 firstJulia<-function (){
- warning('Remember using: options(JULIA_HOME ="C:/path_to_the_bin/Julia 1.5.1/bin)"')
+ warning('Remember using: options(JULIA_HOME ="C:/path_to_the_bin/Julia 1.5.1/bin" )')
 library(JuliaCall)
 julia_setup()
 julia_command("
