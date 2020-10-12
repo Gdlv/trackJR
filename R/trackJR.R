a98ae8c41974456570cd7f5b80811cb8210896f6
GERARDO DE LA VEGA <delavega.gerardo@gmail.com>
2020-10-12 15:22:48 -0300

    add repo

---
+++ R/trackJR.R
@@ -0,0 +1,27 @@
+#' trackJR
+#'@description Tracking a video of tiny insect to get frame, position X and position Y.
+#' @param pathDirAndFile Path of the directory and the video file name.is a Character string; e.g. "C:myPC/video.mp4".
+#'
+#' @return (data.frame) It return the tracked video with 3 column, the frame with x and y position. Be careful with X and Y position in the video, frames could be had (0,0) origin position at top-left of the plot.
+#' @export
+#'
+#' @examples
+#'mypathDirandFile<-"C:/Users/test/vid.mp4"
+#'trackJR(mypathDirandFile)
+#'
+trackJR<- function (pathDirAndFile){
+  VFile<-pathDirAndFile
+  pathDirTemp<-paste(tempdir(),"/trackJRtemp/",sep="")
+
+    if(!file.exists(pathDirTemp)) {  # If the folder does not exist, create a new one
+      dir.create(pathDirTemp,F)
+    } else {   # If it existed, delete and replace with a new one
+      unlink(pathDirTemp, recursive = TRUE)
+      dir.create(pathDirTemp,F)
+    }
+
+    system(paste("ffmpeg -i ",VFile," ",pathDirTemp,"%d.png",sep=""))
+    a<-JuliaCall::julia_call("trackingR", pathDirTemp)
+    return(a)
+}
+
---
+++ R/trackJR_Batch.R
@@ -0,0 +1,29 @@
+#' trackJR_Batch
+#'@description Tracking a video of tiny insect to get frame, position X and position Y.In a batch mode.
+#' @param pathDir Path of the directory where videos saved.is a Character string; e.g. "C:myPC/videos". write the string without last bar "/"
+#'
+#' @return (list) It return the tracked directory in a list with all the videos with 3 column, the frame with x and y position. Be careful with X and Y position in the video, frames could be had (0,0) origin position at top-left of the plot.
+#' @export
+#'
+#' @examples
+#'mypathDir<-"C:/Users/test" # write the string without last bar "/"
+#'trackJR_Batch(mypathDir)
+#'
+trackJR_Batch<- function (pathDir){
+  listF<-as.list(list.files(pathDir))
+  names(listF)=listF
+  for(i in 1:length(listF)){
+    pathDirTemp<-paste(tempdir(),"/",names(listF)[i],"/",sep="")
+
+    if(!file.exists(pathDirTemp)) {  # If the folder does not exist, create a new one
+      dir.create(pathDirTemp,F)
+    } else {   # If it existed, delete and replace with a new one
+      unlink(pathDirTemp, recursive = TRUE)
+      dir.create(pathDirTemp,F)
+    }
+
+    system(paste("ffmpeg -i ",pathDir,"/",names(listF)[i]," ",pathDirTemp,"%d.png",sep=""))
+    listF[[i]]<-JuliaCall::julia_call("trackingR", pathDirTemp)
+    }
+  return(listF)
+}
