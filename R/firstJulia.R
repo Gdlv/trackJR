c50dd4fd8a072fa22db20822cfe09543d1e4ada5
GERARDO DE LA VEGA <delavega.gerardo@gmail.com>
2020-10-12 17:41:19 -0300

    first julia

--- R/firstJulia.R
+++ R/firstJulia.R
@@ -1,3 +1,15 @@
+#' firstJulia
+#'@description First step to check and start using Julia function for tracking tiny insect
+#' @param no parameters need
+#'
+#' @return It return the linked function of Julia
+#' @export
+#'
+#' @examples
+#' #Just type the function
+#' firstJUlia()
+#'
+
 firstJulia<-function (){
 warning('Remember using: options(JULIA_HOME ="C:/path_to_the_bin/Julia 1.5.1/bin" )')
 library(JuliaCall)
