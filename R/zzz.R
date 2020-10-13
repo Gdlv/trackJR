3819495702fda118d6466007b3c36098d4ea1cc7
GERARDO DE LA VEGA <delavega.gerardo@gmail.com>
2020-10-12 23:10:07 -0300

    onLoad!

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-.onAttach <- function(libname, pkgname) {
+.onLoad <- function(libname, pkgname) {
   packageStartupMessage('Remember using: options(JULIA_HOME ="C:/path_to/Julia 1.x.x/bin")')
 
   JuliaCall::julia_setup()
