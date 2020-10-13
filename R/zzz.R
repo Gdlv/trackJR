c5e2ea5633e669f65488793ab059e68a055246bf
GdelaVega <6731226+Gdlv@users.noreply.github.com>
2020-10-13 13:45:07 -0300

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,8 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage('Remember using: options(JULIA_HOME ="C:/path_to/Julia 1.x.x/bin")')
+  
+  packageStartupMessage('------------------------------------------------------------------------------ \t
+                           visit JuliaCall for troubleshoting or https://github.com/Gdlv/trackJR/
+                           ------------------------------------------------------------------------------ \t
+                           Start Julia in R using "firstJulia()"')
 }
