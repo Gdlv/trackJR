5079580f39e19f787963b4a7231ef40045c1767a
GERARDO DE LA VEGA <delavega.gerardo@gmail.com>
2020-10-22 09:37:01 -0300

    setting new param

--- R/trackJR.R
+++ R/trackJR.R
@@ -1,7 +1,8 @@
 #' trackJR
 #'@description Tracking a video of tiny insect to get frame, position X and position Y.
 #' @param pathDirAndFile Path of the directory and the video file name.is a Character string; e.g. "C:myPC/video.mp4".
-#'
+#' @param rframes Rate of frame which you want to extract or transform the video. Default is 20.
+#' @param timestop Video time duration for tracking. Default is 5 minutes. Bring as a character string "00:05:00".
 #' @return (data.frame) It return the tracked video with 3 column, the frame with x and y position. Be careful with X and Y position in the video, frames could be had (0,0) origin position at top-left of the plot.
 #' @export
 #'
@@ -9,7 +10,7 @@
 #'mypathDirandFile<-"C:/Users/test/vid.mp4"
 #'trackJR(mypathDirandFile)
 #'
-trackJR<- function (pathDirAndFile){
+trackJR<- function (pathDirAndFile,rframe=20, timestop="00:05:00"){
   VFile<-pathDirAndFile
   pathDirTemp<-paste(tempdir(),"/trackJRtemp/",sep="")
 
@@ -20,7 +21,7 @@ trackJR<- function (pathDirAndFile){
       dir.create(pathDirTemp,F)
     }
 
-    system(paste("ffmpeg -i ",VFile," ",pathDirTemp,"%d.png",sep=""))
+    system(paste("ffmpeg -i ",VFile,"-r ",rframes," -ss ",timestop,pathDirTemp,"%d.png",sep=""))
     a<-JuliaCall::julia_call("trackingR", pathDirTemp)
     return(a)
 }
--- R/trackJR_Batch.R
+++ R/trackJR_Batch.R
@@ -1,7 +1,9 @@
 #' trackJR_Batch
 #'@description Tracking a video of tiny insect to get frame, position X and position Y.In a batch mode.
 #' @param pathDir Path of the directory where videos saved.is a Character string; e.g. "C:myPC/videos". write the string without last bar "/"
-#'
+#' @param rframes Rate of frame which you want to extract or transform the video. Default is 20.
+#' @param timestop Video time duration for tracking. Default is 5 minutes. Bring as a character string "00:05:00".
+
 #' @return (list) It return the tracked directory in a list with all the videos with 3 column, the frame with x and y position. Be careful with X and Y position in the video, frames could be had (0,0) origin position at top-left of the plot.
 #' @export
 #'
@@ -9,7 +11,7 @@
 #'mypathDir<-"C:/Users/test" # write the string without last bar "/"
 #'trackJR_Batch(mypathDir)
 #'
-trackJR_Batch<- function (pathDir){
+trackJR_Batch<- function (pathDir,rframes=20,timestop="00:05:00"){
   listF<-as.list(list.files(pathDir))
   names(listF)=listF
   for(i in 1:length(listF)){
@@ -22,7 +24,7 @@ trackJR_Batch<- function (pathDir){
       dir.create(pathDirTemp,F)
     }
 
-    system(paste("ffmpeg -i ",pathDir,"/",names(listF)[i]," ",pathDirTemp,"%d.png",sep=""))
+    system(paste("ffmpeg -i ",pathDir,"/",names(listF)[i]," -r ",rframes," -ss ",timestop," ",pathDirTemp,"%d.png",sep=""))
     listF[[i]]<-JuliaCall::julia_call("trackingR", pathDirTemp)
     }
   return(listF)
